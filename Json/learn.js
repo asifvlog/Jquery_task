674e53fc429d6d87263b215d263ed5ad1b6ff3af
asifvlog <56586948+asifvlog@users.noreply.github.com>
2021-09-01 20:01:09 +0530

    Add files via upload

---
+++ Json/learn.js
@@ -0,0 +1,13 @@
+// let student={
+//     name:"asif",
+//     age:26,
+//     mobile:1234554321
+// };
+
+// let ans = JSON.stringify(student);
+// console.log(ans);
+
+let student1='{"name":"asif","age":26,"mobile":123455432}';
+
+let ans1 = JSON.parse(student1);
+console.log(ans1);
